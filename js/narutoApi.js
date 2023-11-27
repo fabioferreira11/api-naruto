2dce2378d75cd394c7fb7e0db16315c1b40a0abc
fabioferreira11 <145472440+fabioferreira11@users.noreply.github.com>
2023-11-27 15:33:36 +0100

    first commit

--- js/narutoApi.js
+++ js/narutoApi.js
@@ -1,7 +1,8 @@
 $(function(){
     console.log("ready");
 
-   var url = 'https://www.narutodb.xyz/api/akatsuki';
+//    var url = 'https://www.narutodb.xyz/api/akatsuki';
+   var url = 'js/akatsuki.json';
 
    $.ajax({
         type: 'GET',
@@ -16,15 +17,22 @@ $(function(){
         var i =0;
 
 for(i=0;i<monarray.length;i++) {
-    monhtml +='<div class="carte"> <h2>'+ monarray[i].name+'</h2> '
+
+    if(monarray[i].personal.species == undefined) {
+
+        monarray[i].personal.species = "Humain";
+
+    }
+    monhtml +='<div class="carte bg-black text-white w-1/6 h-[42vh] p-3 overflow-auto border-4 border-[#ffffff] rounded-xl my-8 mx-7 grid-item '+monarray[i].personal.species+'"> <h2 class="text-smal">'+ monarray[i].name+'</h2> '
     // monhtml +=  monarray[i].id+'<br>'
+
 
             if(monarray[i].images !== undefined) {
 
                 var ii = 0;
 
                 for(ii=0;ii<monarray[i].images .length;ii++) {
-                    monhtml += '<img src="'+monarray[i].images[ii]+'"> '
+                    monhtml += '<img class="w-full h-3/6 object-cover object-top" src="'+monarray[i].images[ii]+'"> '
                 }
 
                 monhtml += '<br> '
@@ -33,7 +41,7 @@ for(i=0;i<monarray.length;i++) {
             if(monarray[i].jutsu !== undefined) {
 
                 
-                monhtml += '<h3> Jutsu : </h3> <p>'
+                monhtml += '<h3 class="text-xsmal"> Jutsu : </h3> <p>'
 
                 for(ii=0;ii<monarray[i].jutsu .length;ii++) {
                    monhtml += monarray[i].jutsu[ii]+' <br>'
@@ -41,6 +49,8 @@ for(i=0;i<monarray.length;i++) {
 
                 monhtml += '</p> <br>'
             }
+
+            monhtml += '<h3> Espèce : </h3> <p> '+monarray[i].personal.species+'</p> <br> '
 
             if(monarray[i].natureType !== undefined) {
                 monhtml += '<h3> Type : </h3> <p>'
@@ -53,24 +63,25 @@ for(i=0;i<monarray.length;i++) {
                 monhtml += '</p> <br> '
             }
 
-            if(monarray[i].voiceActors !== undefined) {
+            // if(monarray[i].voiceActors !== undefined) {
             
-                monhtml += '<h3> voix des acteurs : </h3> <p>'
-
+            //     monhtml += '<h3> voix des acteurs : </h3> <p>'
+            //     monhtml += monarray[i].voiceActors.japanese[0]+'<br>'
                 
-                for (var key in monarray[i].voiceActors) {
-                    if (monarray[i].voiceActors.hasOwnProperty(key) ) {
-                        console.log(monarray[i].voiceActors[key]);
-                    }
+            //     for (var key in monarray[i].voiceActors) {
+            //         if (monarray[i].voiceActors.hasOwnProperty(key) ) {
+            //             console.log(monarray[i].voiceActors[key]);
+            //         }
                     
         
-                    monhtml +=''+key+' :'+'<br>'
-                }
+            //         monhtml +=''+key+' :'+'<br>'
+            //     }
 
-                monhtml += '</p> <br> '
-            }
+            //     monhtml += '</p> <br> '
+            // }
+           
 
-    monhtml +=  '</div><hr>'
+    monhtml +=  '</div>'
 
 } // fin boucle i
 
@@ -82,6 +93,20 @@ for(i=0;i<monarray.length;i++) {
         //     }
         // }
 
+        var grid = $('.grid').isotope({
+            // options
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows'
+          });
+    
+          $('.filter button').on( 'click', function() {
+            var value = $(this).attr('data-name');
+            console.log("value",value);
+    
+            grid.isotope({ 
+              filter: value
+            });
+           } );//click
         
 
         },// fin success   
